63130dd71bb24896d26605c94429eba14e5fe07e
Ralph Case <ralphcase@hotmail.com>
2024-11-08 14:08:02 -0500

    Add a few comments.

--- Weekly.py
+++ Weekly.py
@@ -5,10 +5,12 @@ from breeze_chms_api import breeze
 # Initialize Breeze API
 breeze_api = breeze.breeze_api(breeze_url=config.church_domain_url, api_key=config.breeze_api_key)
 
+
 def get_pantry_date():
     current_run = datetime.now()
     thursday = current_run + timedelta(days= 3 - current_run.weekday())
     return thursday.strftime('%m/%d/%Y')
+
 
 def fetch_event_attendance(pantry_date):
     events = breeze_api.list_events(start=pantry_date, end=pantry_date, category_id=55532, details=0)
@@ -16,12 +18,15 @@ def fetch_event_attendance(pantry_date):
         raise ValueError(f"No events found on {pantry_date}")
     return breeze_api.list_attendance(instance_id=events[0]['id'])
 
-def get_total_people_and_families(attendance, base_count):
-    total_people = base_count
+
+def get_total_people_and_families(attendance):
+    total_people = 0
     total_families = len(attendance)
     
     for person in attendance:
         profile = breeze_api.get_person_details(person_id=person['person_id'])
+
+        # This is the Breeze key for the profile field "How many people live in your household? (including yourself)"
         family_size_key = '1515006285'
         
         if family_size_key in profile['details']:
@@ -31,14 +36,19 @@ def get_total_people_and_families(attendance, base_count):
     
     return total_people, total_families
 
+
 def main():
     pantry_date = get_pantry_date()
     attendance = fetch_event_attendance(pantry_date)
     
+    total_people, total_families = get_total_people_and_families(attendance)
+    
+    # Estimate of the number of seniors fed from the food distributed at the Sunrise Center.
     base_senior_count = 25
-    total_people, total_families = get_total_people_and_families(attendance, base_senior_count)
+    total_people += base_senior_count
     
     print(f'On {pantry_date}, we fed {total_people} people and {total_families} families.')
+
 
 if __name__ == "__main__":
     main()
